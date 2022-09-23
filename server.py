8af2ab92026a2cb77bb692e1bb3e25ee18ff39e9
aidan1156 <86884697+aidan1156@users.noreply.github.com>
2022-09-23 21:13:38 +0100

    more work

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect,make_response, send_from_directory
 import os
 import json
+import server_query
 
 main_dir = os.path.dirname(os.path.realpath(__file__))#path to the server (paths referenced in the code are relative to this)
 
@@ -12,6 +13,12 @@ app.config['PREFERRED_URL_SCHEME'] = 'https:'
 def home():
     if request.method == 'GET':
         return render_template('home.html')
+    if request.method == 'POST':##handle requests to edit/get data
+        data = json.loads(request.data)
+        if data['request_type'] == 'get_designs':
+            user_id = server_query.token_to_user_id(data['user_token'],request.cookies.get('user_token'))
+            designs = server_query.get_designs(user_id)
+            return json.dumps({'state':'done','designs':designs})
 
 @app.route('/sign-in',methods=['GET','POST'])
 def sign_in():
@@ -19,7 +26,14 @@ def sign_in():
         return render_template('sign-in.html')
     if request.method == 'POST':
         data = json.loads(request.data)
-        return json.dumps({'gg':'dfg'})
+        user_id = server_query.check_password(data['username'],data['password'])
+        if user_id:
+            token,cookie_token = server_query.create_token(user_id)
+            resp = make_response(json.dumps({'state':'done','user_token':token}))##set the user token cookie that lasts a year
+            resp.set_cookie('user_token', cookie_token,httponly = True,max_age=31536000, secure=True,samesite='Strict')
+            return resp
+        else:
+            return json.dumps({'state':'Incorrect password or username'})
 
 if __name__ == '__main__':
     app.run(debug=True,port=5003)
---
+++ server_query.py
@@ -0,0 +1,119 @@
+import sqlite3
+import hashlib
+import string
+import random
+
+def hash_data(data):
+    encoded = data.encode()
+    result = hashlib.sha256(encoded)
+    return result.hexdigest()
+
+def clean_for_sql(data):
+    return data.replace("'","''")
+
+##check if the given usernam matches the password in the db
+def check_password(username,password):
+    password = hash_data(password)
+    username = clean_for_sql(username)
+
+    conn = sqlite3.connect("users.db")
+    cur = conn.cursor()
+
+    sql= f'''
+        SELECT user_id FROM users 
+        WHERE username='{username}' AND password='{password}'
+    '''
+
+    cur.execute(sql)
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    if result:
+        return result[0][0]
+    
+    return False
+
+def create_token(user_id):
+    ##create and insert a new set of user tokens into the db
+    token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(32))
+    hashed_token = hash_data(token)
+
+    cookie_token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(32))
+    hashed_cookie_token = hash_data(cookie_token)
+
+    conn = sqlite3.connect("users.db")
+    cur = conn.cursor()
+
+    sql= f'''
+        INSERT INTO tokens
+        VALUES ({user_id},'{hashed_token}','{hashed_cookie_token}')
+    '''
+
+    try:##if this fails it means we didnt generate a unique set of tokens
+        cur.execute(sql)
+    except:##so try again
+        token, cookie_token = create_token(user_id)
+    finally:
+        cur.close()
+        conn.commit()
+        conn.close()
+
+    return token,cookie_token
+
+def token_to_user_id(token,cookie_token):
+    token = hash_data(token)
+    cookie_token = hash_data(cookie_token)
+
+    conn = sqlite3.connect("users.db")
+    cur = conn.cursor()
+
+    sql= f'''
+        SELECT FK_user_id FROM tokens 
+        WHERE user_token='{token}' AND cookie_token='{cookie_token}'
+    '''
+
+    cur.execute(sql)
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    if result:
+        return result[0][0]
+    
+    return False
+
+def get_designs(user_id):
+    conn = sqlite3.connect("users.db")
+    cur = conn.cursor()
+
+    sql= f'''
+        SELECT design_id FROM designs 
+        WHERE FK_user_id='{user_id}'
+        ORDER BY edited_on DESC
+    '''
+
+    cur.execute(sql)
+    result = cur.fetchall()
+
+    data = []
+    for i in range(len(result)):
+        sql= f'''
+            SELECT x,y FROM design_pattern 
+            WHERE FK_design_id='{result[i][0]}'
+        '''
+
+        cur.execute(sql)
+        pattern = cur.fetchall()
+
+        data.append({
+            'design_id':result[i][0],
+            'pattern':pattern
+        })
+
+    cur.close()
+    conn.close()
+
+    return data
+    
+    
