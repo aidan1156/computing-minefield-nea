34b3002885166e2c70075adc241cad2893a2e06c
aidan1156 <86884697+aidan1156@users.noreply.github.com>
2022-12-16 17:47:59 +0000

    added button pressing!

--- arduino code/test/test.ino
+++ arduino code/test/test.ino
@@ -1,20 +1,29 @@
 //import the software serial port and define the pins
 #include <SoftwareSerial.h>
+#include <stdlib.h>
 #define rxPin 2
 #define txPin 3
 #define ledPin 4
 #define buttonpin 5
 
+#define message_max_length 30//the max length of a message 
+#define bluetooth_tick_speed 100//how often (in ms) we should send the currently pressed buttons
+#define button_tick_speed 50//how often we should check if the buttons are clicked
+
 //setup the software serial port
 SoftwareSerial mySerial(rxPin, txPin); // RX, TX
 
 //define the variables
+int i;
+int n;
+
 bool senddata = false;
-char s[50];
 char command;
-int tick_time = millis();
-int button_tick_time = millis();
-int button_states[64];
+unsigned long tick_time = millis();
+unsigned long button_tick_time = millis();
+int button_states[64];//state of all 64 buttons
+char bluetooth_message[message_max_length];//string which is sent to app
+int message_buffer_pos;
 
 void setup() {
     //setup the pins
@@ -27,45 +36,70 @@ void setup() {
 
     //start the serial connections
     mySerial.begin(115200); 
-    mySerial.println ("Hello");
-
     Serial.begin(9600);
 }
 
 void loop() {
     //if 700 miliseconds have passed since we last checked for messages/sent messages
-    if (millis()-tick_time > 700){
+    if (millis()-tick_time > bluetooth_tick_speed){
+        tick_time = millis();
         //check for updates
         while (mySerial.available()>0) {
             command=mySerial.read ();
             if (command == 'g'){
-            senddata = true;
+                senddata = true;
             }
             if (command == 's'){
-            senddata = false;
+                senddata = false;
             }
         }
-        //send data
+
         if (senddata){
-            sprintf (s,"%d,%d,%d\r",1,1,3);
-            mySerial.println (s); 
+            //loop through the 'bluetooth_message' array and clear it ready for next input
+            for (i=0;i<message_max_length;i++){
+                bluetooth_message[i] = 0;
+            }
+
+            button_states[1] = 5;
+
+            message_buffer_pos = 0;//reset the pos variable
+            for (i=0;i<64;i++){//loop over each button
+                //if the current pos is less than the max pos-3 (due to us using a max of 3 buffer points in 1 loop)
+                if (message_buffer_pos < message_max_length-3){
+                    if (button_states[i] > 0){//if the button has been pressed for more than 3 ticks
+                        char button_id[4];//convert i into a string 
+                        itoa(i,button_id,10);
+                        for (n=0;n<strlen(button_id);n++){//then loop over each char in the button id
+                            bluetooth_message[message_buffer_pos] = button_id[n];//and add it to the message string
+                            message_buffer_pos += 1;
+                        }
+                        bluetooth_message[message_buffer_pos] = ',';//comma separate it
+                        message_buffer_pos += 1;
+                    }
+                }
+            }
+            if (message_buffer_pos != 0){
+                bluetooth_message[message_buffer_pos-1] = 0;//add the string terminator character
+                Serial.println(bluetooth_message);//send the message
+                mySerial.println(bluetooth_message);
+            }      
+            else{//if nothing is pressed print nothing
+                Serial.println("nothing");
+                mySerial.println("nothing");
+            }
         }
-        tick_time = millis();
     }
 
     //read the button for input changes
-    if (millis()-button_tick_time > 50){
-        if (digitalRead(buttonpin) == true){
+    if (millis()-button_tick_time > button_tick_speed){
+        if (digitalRead(buttonpin) == false){
             button_states[0] += 1; 
         }
-        else if (button_states[0] != 0){
+        else{
             button_states[0] = 0;
-            Serial.println(0);
         }
-        if (button_states[0] == 4){
-            Serial.println(1);
-        }
-
         button_tick_time = millis();
     }
+
+//    delay(500);
 }
